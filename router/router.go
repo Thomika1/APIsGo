92b7d4a66c21ae419c04ea66b6c61a48a39e56ea
Thomika1 <thomazmbonfim@gmail.com>
2025-02-05 18:36:39 -0300

    Feature: Add routes logic get, post, delete, put

--- router/router.go
+++ router/router.go
@@ -5,12 +5,13 @@ import (
 )
 
 func Initialize() {
-	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080
-}
+	// Initialize router
+	router := gin.Default()
+
+	// Initialize Routes
+	initializeRoutes(router)
+
+	// Run the server
+	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+} // initialize router
