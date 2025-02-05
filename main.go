097c9c592ee8e5867a7ecc97cb0aebe92863a861
Thomika1 <thomazmbonfim@gmail.com>
2025-02-05 17:37:52 -0300

    Feature: add router subpackage

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
+import "github.com/Thomika1/APIsGo.git/router"
 
 func main() {
-	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080
+	router.Initialize()
+
 } // MAIN
