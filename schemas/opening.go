51cd0eca2ca7b197d7a508bd7a2fda0d611ae8a4
Thomika1 <thomazmbonfim@gmail.com>
2025-02-07 20:07:47 -0300

    Feature: Added opneing response struct

--- schemas/opening.go
+++ schemas/opening.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -13,3 +15,16 @@ type Opening struct {
 	Link     string
 	Salary   int64
 } // struc opening
+
+type OpeningResponse struct {
+	ID        uint      `json: "id"`
+	CreatedAt time.Time `json: "createdat"`
+	UpdatedAt time.Time `json: "updatedat"`
+	DeletedAt time.Time `json: "deletedat"`
+	Role      string    `json: "role"`
+	Company   string    `json: "company"`
+	Location  string    `json: "location"`
+	Remote    bool      `json: "remote"`
+	Link      string    `json: "link"`
+	Salary    int64     `json: "salary"`
+}
