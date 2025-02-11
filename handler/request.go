140aed62dcf659760f2f0c62945491bfe9add406
Thomika1 <thomazmbonfim@gmail.com>
2025-02-11 14:54:26 -0300

    Feature: updated request to handle update validation

--- handler/request.go
+++ handler/request.go
@@ -19,7 +19,7 @@ type CreateOpeningRequest struct {
 
 func (r *CreateOpeningRequest) Validate() error {
 	if r.Role == "" && r.Company == "" && r.Location == "" && r.Link == "" && r.Remote == nil && r.Salary <= 0 {
-		return fmt.Errorf("request body is empty")
+		return fmt.Errorf("request body is empty or malformed")
 	}
 
 	if r.Role == "" {
@@ -46,4 +46,23 @@ func (r *CreateOpeningRequest) Validate() error {
 		return errParamIsRequired("salary", "int64")
 	}
 	return nil
+}
+
+// Update opening
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+} // struct
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Link != "" || r.Remote != nil || r.Salary <= 0 {
+		return nil
+	}
+	// If none of thw fields were provided return false
+	return fmt.Errorf("at least one field valid must be provided")
 }
