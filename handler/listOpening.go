30153f4c8aa08cf7e788a376d2c7db826cab11c9
Thomika1 <thomazmbonfim@gmail.com>
2025-02-11 14:53:11 -0300

    Feature: Added list openings handler

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -3,11 +3,18 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Thomika1/APIsGo.git/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-opening", openings)
+
 } // Func List Opening handler
